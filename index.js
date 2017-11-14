25469064c8b912bf1018ea61d43fe04637af408a
raj2sudha <raj2sudha1@gmail.com>
2017-11-14 16:54:31 +0000

    create this helloworld app, to test the Update Apps when their is a change in git hub

---
+++ index.js
@@ -0,0 +1,9 @@
+process.title="HelloApp"
+console.log("Hello World");
+
+//process.on('SIGINT', console.log("SignOut from HelloApp"));
+
+
+// Prevents the program from closing instantly
+process.stdin.resume();
+
