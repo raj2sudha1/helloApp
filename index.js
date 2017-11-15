4d03f5f641af817f4de781ea678c96b49592c07a
Sudhakar <raj2sudha1@gmail.com>
2017-11-15 10:26:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 process.title="HelloApp"
 console.log("Hello World");
 
+//TODO : test the below code for npm stop command
 //process.on('SIGINT', console.log("SignOut from HelloApp"));
 
 // Prevents the program from closing instantly
