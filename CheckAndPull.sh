90e0a9aa69f79b5c51be98116a2424f1dd7d14f7
raj2sudha1 <raj2sudha1@gmail.com>
2017-11-21 11:57:15 +0000

    updated the shell script

--- CheckAndPull.sh
+++ CheckAndPull.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
 
+#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo "fetch Latest"
 git fetch --prune
 
 UPSTREAM=${1:-'@{u}'}
@@ -8,14 +10,14 @@ LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
 BASE=$(git merge-base @ "$UPSTREAM")
 
-if [ $LOCAL = $REMOTE ]; then
-    echo "Up-to-date"
-elif [ $LOCAL = $BASE ]; then
+if [ $LOCAL != $REMOTE ]; then
+    #echo "Up-to-date"
+#elif [ $LOCAL = $BASE ]; then
     echo "Update available and downloading..."
     npm stop && git reset --hard && git clean -dfx && git pull && npm start &
     #echo "Need to pull"
-elif [ $REMOTE = $BASE ]; then
-    echo "Need to push"
-else
-    echo "Diverged"
+#elif [ $REMOTE = $BASE ]; then
+#   echo "Need to push"
+#else
+#   echo "Diverged"
 fi
