b71d76080604b6cd24f2544e7be2c736d6a0ebdc
Sudhakar <raj2sudha1@gmail.com>
2017-11-21 18:22:09 +0530

    update index.js - 5

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.title="HelloApp"
-console.log("Hello World 4");
+console.log("Hello World 5");
 
 //TODO : test the below code for npm stop command
 //process.on('SIGINT', console.log("SignOut from HelloApp"));
