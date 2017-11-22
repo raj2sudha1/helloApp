06e08c4870440f7ea6510e52e4251fafdfc19fdd
raj2sudha1 <raj2sudha1@gmail.com>
2017-11-22 03:35:51 +0000

    Code Cleanup in CheckAndPull.sh file

--- CheckAndPull.sh
+++ CheckAndPull.sh
@@ -1,23 +1,13 @@
 #!/bin/sh
 
-
-#DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-echo "fetch Latest"
 git fetch --prune
 
 UPSTREAM=${1:-'@{u}'}
 LOCAL=$(git rev-parse @)
 REMOTE=$(git rev-parse "$UPSTREAM")
-BASE=$(git merge-base @ "$UPSTREAM")
+
 
 if [ $LOCAL != $REMOTE ]; then
-    #echo "Up-to-date"
-#elif [ $LOCAL = $BASE ]; then
-    echo "Update available and downloading..."
+   # echo "Update available and downloading..."
     npm stop && git reset --hard && git clean -dfx && git pull && npm install && npm start &
-    #echo "Need to pull"
-#elif [ $REMOTE = $BASE ]; then
-#   echo "Need to push"
-#else
-#   echo "Diverged"
 fi
