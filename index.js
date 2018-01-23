1583afcf2050f18ba02e9c136199672d2243e37f
Sudhakar <raj2sudha1@gmail.com>
2018-01-23 11:33:21 +0530

    Update index.js - 9

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 process.title="HelloApp"
-console.log("Hello World 8");
+console.log("Hello World 9");
 
 //TODO : test the below code for npm stop command
 process.on("SIGINT", () => {console.log("SignOut from HelloApp");});
